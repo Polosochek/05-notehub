3ac7882f5dda697c804cf2823e0dc1078325278f
Polosok <rpolubotka865@gmail.com>
2026-02-18 17:19:02 +0200

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
-import App from './componets/app/App'
+import App from './components/app/App'
 
 const queryClient = new QueryClient()
 
